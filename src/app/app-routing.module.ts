79faf4513262e2614a0fa94e6f934be7b174b61c
Shubham Singh <shubham.singh12@infosys.com>
2019-10-08 21:15:55 +0530

    Routes Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostEditComponent } from './components/post-edit/post-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: "full",
+    redirectTo: "home"
+  },
+  {
+    path: 'home',
+    component: PostListComponent
+  },
+  {
+    path: 'edit',
+    component: PostEditComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
